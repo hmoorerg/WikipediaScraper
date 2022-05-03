e1105f6ac6dce405b43d8fd1480fadfdd15c1c5a
kevnguy <56405177+kevnguy@users.noreply.github.com>
2022-05-02 19:37:08 -0700

    Add mongo pipeline

--- wikipedia_scraper/pipelines.py
+++ wikipedia_scraper/pipelines.py
@@ -6,7 +6,57 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+from scrapy.utils.project import get_project_settings
+import logging
+from scrapy.exceptions import DropItem
 
+log = logging.getLogger(__name__)
+settings = get_project_settings()
+
+class MongoDBPipeline(object):
+
+    def __init__(self, mongo_server, mongo_port):
+        self.mongo_server = mongo_server
+        self.mongo_port = mongo_port
+        self.url_seen = set()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        ## pull in information from settings.py
+        return cls(
+            mongo_server = crawler.settings.get('MONGODB_SERVER'),
+            mongo_port = crawler.settings.get('MONGODB_PORT')
+        )
+
+    def open_spider(self, spider):
+        ## initializing spider
+        ## opening db connection
+        self.client = pymongo.MongoClient(
+            settings.get('MONGODB_SERVER'),
+            settings.get('MONGODB_PORT')
+        )
+        self.db = self.client[settings.get('MONGODB_DB')]
+        self.collection = self.db[settings.get('MONGODB_COLLECTION')]
+        #create index on url to keep unique
+        self.collection.create_index([("url", pymongo.ASCENDING)],unique = True)   
+
+    def close_spider(self, spider):
+        ## clean up when spider is closed
+        self.client.close()
+
+    def process_item(self, item, spider):
+        try:
+            self.collection.insert_one(item)
+        except:
+            raise DropItem("Duplicate item title found: %s" % item)
+
+        # if item['url'] in self.url_seen:
+        #     raise DropItem("Duplicate item title found: %s" % item)
+        # else:
+        #     self.url_seen.add(item['url'])
+        #     self.collection.update_one(item,upsert=True)
+        #     return item
 
 class WikipediaScraperPipeline:
     def process_item(self, item, spider):
--- wikipedia_scraper/spiders/wikiscrape.py
+++ wikipedia_scraper/spiders/wikiscrape.py
@@ -1,26 +1,25 @@
 import scrapy
 import logging
-import json
-import pymongo
-
+import sys
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 log = logging.getLogger(__name__)
 
-#mongoDB connect 
-mongo = pymongo.MongoClient(port=27017)
-db = mongo.Wikipedia
-collection = db.Pages
-#create index on url to keep unique 
-collection.create_index([("url", pymongo.ASCENDING)],unique = True)
+#mongoDB connect DONE in settings/pipeline
 
 class WikiSpider1(scrapy.Spider):
+    custom_settings = {}
+    def __init__(self, filename=None):
+        if filename:
+            with open(filename, 'r') as f:
+                self.start_urls = [url.strip() for url in f.readlines()]
+        
     name = "wiki"
     allowed_domains = ['wikipedia.org']
-    start_urls = [
-        'https://en.wikipedia.org/wiki/Dog'
-    ]
+    print(custom_settings)
+    # start_urls = ['https://en.wikipedia.org/wiki/Dog']
+    #link extractor rule doesnt really work
 
     def parse(self, response):
         log.info('Parse function called on %s', response.url)
@@ -31,148 +30,102 @@ class WikiSpider1(scrapy.Spider):
         if editDate:
             editDate = response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
 
-        scraped = {
-                'url': response.url,
-                'title': response.css('title::text').get(),
-                'editDate': editDate,
-                'TOC': response.css('.toc .toctext::text').getall(),
-                'mainText': ' '.join(response.css('#content p::text, #content a::text').re('[A-Za-z0-9\-]+')).lower(),
-                'boldText': ' '.join(response.css('#content p b::text').re('.{2,}')).lower(),
-                'italicText': ' '.join(response.css('#content p i::text').re('.{2,}')).lower()
-                }
-        try:
-            # with open("pages/"+filename,"w") as f:
-            #     json.dump(scraped,f)
-            collection.insert_one(scraped)
-        except:
-            log.debug("Error creating file")
-        
-        # Testing stuff some explainations on regex chosen
-        #---------------------------------------------------------------------------------------------
-        # mainText = response.css('p::text').getall()
-        #get all bold text 2+ characters because wiki does a,b,c,d list
-        # boldText = response.css('b::text').re('\w{2,}')
+        yield {
+            'url': response.url,
+            'title': response.css('title::text').get(),
+            'editDate': editDate,
+            'TOC': response.css('.toc .toctext::text').getall(),
+            'mainText': ' '.join(response.css('#content p::text, #content a::text').re('[A-Za-z0-9\-]+')).lower(),
+            'boldText': ' '.join(response.css('#content p b::text').re('.{2,}')).lower(),
+            'italicText': ' '.join(response.css('#content p i::text').re('.{2,}')).lower()
+        }
 
-        # title of wiki page
-        # titleArticle = response.css('#firstHeading::text').get()
-        #title of website
-        # titlePage = response.css('title::text').get()
-        # tocText = response.css('.toc .toctext::text').getall()
-
-        # regex for only /wiki/ links
-        # not too worried about duplicates because will be putting in set later
-        #---------------------------------------------------------------------------------------------
-
-        #---------------------------------------------------------------------------------------------
-        # This yields write to one file with command "scrapy crawl wiki -O wiki.json" 
-        #            -O: append to file 
-        #            -O: create new file
-        #            instead of yield we can just insert into mongo
-
-        #prob need to do some error checking if null
-        # try:
-            # yield{
-            #     'id': hashlib.sha256(str(response.url).encode('utf-8')).hexdigest()
-            #     'url': response.url,
-            #     'title': response.css('#firstHeading::text').get(),
-            #     'mainText': ' '.join(response.css('p::text').re('[^\n].*')),
-            #     'boldText': response.css('b::text').re('.{2,}'),
-            #     'TOC': response.css('.toc .toctext::text').getall(),
-            #     'editDate': response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
-            # }
-        # except:
-        #     log.debug("Yikes")
-        #---------------------------------------------------------------------------------------------
-
+        #------------------------------------------------------------------------------------------
         #Get all href within content body
-        hrefText = response.css('#content a::attr(href)').re('/wiki/.*')
+        hrefText = response.css('#content a::attr(href)').re(r'^/wiki/.*(?<![.](?:jpg|png|svg))$')
         # joins href with domain 
         nextPages = set([response.urljoin(link) for link in hrefText])
-        # TODO: follow next link \
-        for page in nextPages:
-            yield response.follow(page, callback=self.parse)
+        #------------------------------------------------------------------------------------------
+        yield from response.follow_all(nextPages, callback=self.parse)
 
-class WikiSpider2(scrapy.Spider):
-    name = "wiki2"
-    allowed_domains = ['wikipedia.org']
-    start_urls = [
-        'https://en.wikipedia.org/wiki/Cat'
-    ]
+# class WikiSpider2(scrapy.Spider):
+#     name = "wiki2"
+#     allowed_domains = ['wikipedia.org']
 
-    def parse(self, response):
-        log.info('Parse function called on %s', response.url)
-        page = response.url.split('/')[-1]
-        filename = 'wiki-%s.json' % page
+#     def parse(self, response):
+#         log.info('Parse function called on %s', response.url)
+#         page = response.url.split('/')[-1]
+#         filename = 'wiki-%s.json' % page
 
-        editDate = response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')
-        if editDate:
-            editDate = response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
+#         editDate = response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')
+#         if editDate:
+#             editDate = response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
 
-        scraped = {
-                'url': response.url,
-                'title': response.css('title::text').get(),
-                'editDate': editDate,
-                'TOC': response.css('.toc .toctext::text').getall(),
-                'mainText': ' '.join(response.css('#content p::text, #content a::text').re('[A-Za-z0-9\-]+')).lower(),
-                'boldText': ' '.join(response.css('#content p b::text').re('.{2,}')).lower(),
-                'italicText': ' '.join(response.css('#content p i::text').re('.{2,}')).lower()
-                }
-        try:
-            # with open("pages/"+filename,"w") as f:
-            #     json.dump(scraped,f)
-            collection.insert_one(scraped)
-        except:
-            log.debug("Error creating file")
+#         yield {
+#             'url': response.url,
+#             'title': response.css('title::text').get(),
+#             'editDate': editDate,
+#             'TOC': response.css('.toc .toctext::text').getall(),
+#             'mainText': ' '.join(response.css('#content p::text, #content a::text').re('[A-Za-z0-9\-]+')).lower(),
+#             'boldText': ' '.join(response.css('#content p b::text').re('.{2,}')).lower(),
+#             'italicText': ' '.join(response.css('#content p i::text').re('.{2,}')).lower()
+#         }
+
+#         # try:
+#         #     # with open("pages/"+filename,"w") as f:
+#         #     #     json.dump(scraped,f)
+#         #     collection.insert_one(scraped)
+#         # except:
+#         #     log.debug("Error creating file")
         
-        # Testing stuff some explainations on regex chosen
-        #---------------------------------------------------------------------------------------------
-        # mainText = response.css('p::text').getall()
-        #get all bold text 2+ characters because wiki does a,b,c,d list
-        # boldText = response.css('b::text').re('\w{2,}')
+        
+#         # Testing stuff some explainations on regex chosen
+#         #---------------------------------------------------------------------------------------------
+#         # mainText = response.css('p::text').getall()
+#         #get all bold text 2+ characters because wiki does a,b,c,d list
+#         # boldText = response.css('b::text').re('\w{2,}')
 
-        # title of wiki page
-        # titleArticle = response.css('#firstHeading::text').get()
-        #title of website
-        # titlePage = response.css('title::text').get()
-        # tocText = response.css('.toc .toctext::text').getall()
+#         # title of wiki page
+#         # titleArticle = response.css('#firstHeading::text').get()
+#         #title of website
+#         # titlePage = response.css('title::text').get()
+#         # tocText = response.css('.toc .toctext::text').getall()
 
-        # regex for only /wiki/ links
-        # not too worried about duplicates because will be putting in set later
-        #---------------------------------------------------------------------------------------------
+#         # regex for only /wiki/ links
+#         # not too worried about duplicates because will be putting in set later
+#         #---------------------------------------------------------------------------------------------
 
-        #---------------------------------------------------------------------------------------------
-        # This yields write to one file with command "scrapy crawl wiki -O wiki.json" 
-        #            -O: append to file 
-        #            -O: create new file
-        #            instead of yield we can just insert into mongo
+#         #---------------------------------------------------------------------------------------------
+#         # This yields write to one file with command "scrapy crawl wiki -O wiki.json" 
+#         #            -O: append to file 
+#         #            -O: create new file
+#         #            instead of yield we can just insert into mongo
 
-        #prob need to do some error checking if null
-        # try:
-            # yield{
-            #     'id': hashlib.sha256(str(response.url).encode('utf-8')).hexdigest()
-            #     'url': response.url,
-            #     'title': response.css('#firstHeading::text').get(),
-            #     'mainText': ' '.join(response.css('p::text').re('[^\n].*')),
-            #     'boldText': response.css('b::text').re('.{2,}'),
-            #     'TOC': response.css('.toc .toctext::text').getall(),
-            #     'editDate': response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
-            # }
-        # except:
-        #     log.debug("Yikes")
-        #---------------------------------------------------------------------------------------------
+#         #prob need to do some error checking if null
+#         # try:
+#             # yield{
+#             #     'id': hashlib.sha256(str(response.url).encode('utf-8')).hexdigest()
+#             #     'url': response.url,
+#             #     'title': response.css('#firstHeading::text').get(),
+#             #     'mainText': ' '.join(response.css('p::text').re('[^\n].*')),
+#             #     'boldText': response.css('b::text').re('.{2,}'),
+#             #     'TOC': response.css('.toc .toctext::text').getall(),
+#             #     'editDate': response.css('#footer-info-lastmod').re('\d+[A-Za-z\s]+\d+')[0]
+#             # }
+#         # except:
+#         #     log.debug("Yikes")
+#         #---------------------------------------------------------------------------------------------
 
-        #Get all href within content body
-        hrefText = response.css('#content a::attr(href)').re('/wiki/.*')
-        # joins href with domain 
-        nextPages = set([response.urljoin(link) for link in hrefText])
-        # TODO: follow next link 
-        for page in nextPages:
-            yield response.follow(page, callback=self.parse)
+#         #Get all href within content body
+#         hrefText = response.css('#content a::attr(href)').re('/wiki/.*')
+#         # joins href with domain 
+#         nextPages = set([response.urljoin(link) for link in hrefText])
+#         yield response.follow_all(nextPages, callback=self.parse)
 
 
 if __name__ == '__main__':
     settings = get_project_settings()
     process = CrawlerProcess(settings)
     process.crawl(WikiSpider1)
-    process.crawl(WikiSpider2)
+    #process.crawl(WikiSpider2)
     process.start() 

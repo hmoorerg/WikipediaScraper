e08ad56f49c830191e72a601c7971fb59d2fb0b4
kevnguy <56405177+kevnguy@users.noreply.github.com>
2022-05-04 00:57:44 -0700

    Add local storage pipeline

--- wikipedia_scraper/pipelines.py
+++ wikipedia_scraper/pipelines.py
@@ -6,57 +6,64 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-import pymongo
-from scrapy.utils.project import get_project_settings
-import logging
 from scrapy.exceptions import DropItem
-
-log = logging.getLogger(__name__)
-settings = get_project_settings()
+import pymongo
+import json
 
 class MongoDBPipeline(object):
-
-    def __init__(self, mongo_server, mongo_port):
+    def __init__(self, mongo_server, mongo_port, mongo_db, mongo_collection):
         self.mongo_server = mongo_server
         self.mongo_port = mongo_port
-        self.url_seen = set()
+        self.mongo_db = mongo_db
+        self.mongo_collection = mongo_collection
 
     @classmethod
     def from_crawler(cls, crawler):
         ## pull in information from settings.py
         return cls(
             mongo_server = crawler.settings.get('MONGODB_SERVER'),
-            mongo_port = crawler.settings.get('MONGODB_PORT')
+            mongo_port = crawler.settings.get('MONGODB_PORT'),
+            mongo_db = crawler.settings.get('MONGODB_DB', 'WikiScrape'),
+            mongo_collection = crawler.settings.get('MONGODB_COLLECTION', 'Pages')
         )
 
     def open_spider(self, spider):
-        ## initializing spider
-        ## opening db connection
         self.client = pymongo.MongoClient(
-            settings.get('MONGODB_SERVER'),
-            settings.get('MONGODB_PORT')
+            self.mongo_server,
+            self.mongo_port,
+            connectTimeoutMS=3000,
+            serverSelectionTimeoutMS=3000
         )
-        self.db = self.client[settings.get('MONGODB_DB')]
-        self.collection = self.db[settings.get('MONGODB_COLLECTION')]
+        self.db = self.client[self.mongo_db]
+        self.collection = self.db[self.mongo_collection]
         #create index on url to keep unique
-        self.collection.create_index([("url", pymongo.ASCENDING)],unique = True)   
+        self.collection.create_index([("url", pymongo.ASCENDING)],unique = True)
 
     def close_spider(self, spider):
-        ## clean up when spider is closed
         self.client.close()
 
     def process_item(self, item, spider):
         try:
-            self.collection.insert_one(item)
+            self.collection.update_one({'url': item['url']}, {"$set": item }, upsert=True)
         except:
-            raise DropItem("Duplicate url found: %s" % item['url'])
+            raise DropItem("Error adding item to DB with url: %s" % item['url'])
 
-        # if item['url'] in self.url_seen:
-        #     raise DropItem("Duplicate item title found: %s" % item)
-        # else:
-        #     self.url_seen.add(item['url'])
-        #     self.collection.update_one(item,upsert=True)
-        #     return item
+class StoreLocalPipeline:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            output_dir = crawler.settings.get('output_dir')
+        )
+
+    def process_item(self, item, spider):
+        page = item['url'].split('/')[-1]
+        filename = '%s/wiki-%s.json' % (self.output_dir,page)
+        with open(filename, 'w') as f:
+            json.dump(item,f,default=str)
+        return item
 
 class WikipediaScraperPipeline:
     def process_item(self, item, spider):

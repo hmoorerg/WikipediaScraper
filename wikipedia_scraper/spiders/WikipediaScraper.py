f1205ba8f532bc7f22555af1a05b36d9934c6e30
Henry Moore <hmoorerg@gmail.com>
2022-05-08 01:51:03 -0700

    First broken commit

--- wikipedia_scraper/spiders/WikipediaScraper.py
+++ wikipedia_scraper/spiders/WikipediaScraper.py
@@ -6,14 +6,28 @@ import os
 import urllib
 import base64
 from datetime import datetime
+from scrapy.http import Request
+
+
+class Article(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+    last_modified_date = scrapy.Field()
+    coordinates = scrapy.Field()
+    categories = scrapy.Field()
+    subheaders = scrapy.Field()
+    popularity = scrapy.Field()
+
 
 
 class WikipediaScraper(scrapy.spiders.CrawlSpider):
     name = "wikiscraper"
-    allowed_domains = ["en.wikipedia.org", "www.wiktionary.org", "species.wikimedia.org"]
+    allowed_domains = ["en.wikipedia.org", "wikidata.org"]
     start_urls = ["https://en.wikipedia.org/wiki/Statue_of_Liberty"]
     rules = (
-        Rule(LinkExtractor(allow=(r"wikipedia\.org\/wiki\/[^:]*$", )), callback="parse", follow=True),
+        Rule(LinkExtractor(allow=(r"wikidata.org\/wiki\/", )), follow=False),
+
+        Rule(LinkExtractor(allow=(r"wikipedia\.org\/wiki\/[^:]*$", )), callback="parse"),
     )
 
     def parse(self, response):
@@ -28,16 +42,53 @@ class WikipediaScraper(scrapy.spiders.CrawlSpider):
             coordinates = (latitude, longitude)
 
         subheaders = response.css('span.mw-headline::text').getall()
+        
+        # Combine the data we have so far for this page
+        item = Article()
+        item['title'] = title
+        item['last_modified_date'] = last_modified_date
+        item['coordinates'] = coordinates
+        item['subheaders'] = subheaders
+        item['url'] = response.url
 
-        yield {
-            'Title': title,
-            'Url': response.url,
-            'Last Modified': last_modified_date,
-            'Subheaders': subheaders,
-            'Coordinates': coordinates
-        }
-
+        # Gets the url to the articles's information page    
+        wikimedia_path = response.css('#t-wikibase a::attr(href)').get()
+        page_info_path = response.css('#t-info a::attr(href)').get()
+        
+        # Writes the html file to disk
         self.write_to_disk(response)
+        
+        # Continues parsing by visiting the page information and wikidata pages
+        return response.follow(wikimedia_path, callback=self.parse_wikidata, cb_kwargs = dict(item=item))
+
+    def parse_page_information(self, response, item):
+
+        self.logger.info("Visited %s", response.url)
+        print()
+        print()
+        print("parsing page information")
+        print()
+        print()
+        item["Popularity"] = "Test Value"
+        
+        # Get the link to the wikimedia page
+        
+        # yield response.follow(wikimedia_path, callback=self.parse_wikimedia, cb_kwargs = dict(item=item), dont_filter=True)
+        return item
+
+    def parse_wikidata(self, response, item):
+        print()
+        print()
+        print("parsing page information")
+        print()
+        print()
+
+        self.logger.info("Visited %s", response.url)
+        
+        # Get the type of item that the wikidata page is about
+        item.categories = response.css('#mw-data-entity-type::text').getall()
+        return item
+
 
     def write_to_disk(self, response):
         os.makedirs("./HTML_OUT", exist_ok=True)
